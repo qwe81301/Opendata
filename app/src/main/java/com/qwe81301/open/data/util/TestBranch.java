a140008774a838761ec0e99797c9b2f64077f88c
shih-jin-yu <qwe81301@gmail.com>
2021-06-07 16:45:13 +0800

    (目前測試)test 分支 單commit 合併(4 add new file)

---
+++ app/src/main/java/com/qwe81301/open/data/util/TestBranch.java
@@ -0,0 +1,12 @@
+package com.qwe81301.open.data.util;
+
+/**
+ * author:       bearshih
+ * project:      OpenData
+ * date:         2021/6/7
+ * version:
+ * description:
+ */
+public class TestBranch {
+    //(目前測試)test 分支 單commit 合併(4 add new file)
+}
